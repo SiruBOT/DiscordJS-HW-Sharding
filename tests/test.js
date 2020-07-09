57921ec866498e5f5e1625e297839e1a00802183
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-10 01:11:24 +0900

    WIP - Using Chai's assert, should

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,13 @@
-/* In progress */
+/**
+ * Test Code
+ */
+// const { should, expect, assert } = require('chai/chai')
+// should() // Modification global object
+// describe('Websocket Server', () => {
+//   it('Should Create', (done) => {
+//     const obj = { a: 'a' }
+//     obj.should.key('a')
+//     obj.a.should.to.be.an('array')
+//     done()
+//   })
+// })
