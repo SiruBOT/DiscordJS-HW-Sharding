acc6e433cab4d5eb98853fadee421d274d809fd6
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-10 01:09:34 +0900

    WIP - Checks If Ws instanceof Websocket, Added ID Constructor

--- src/Shard.js
+++ src/Shard.js
@@ -3,11 +3,13 @@ const HwSharderError = require('./HwSharderError')
 class Shard {
   /**
    * Shard Instance
-   * @param {Websocket} ws
+   * @param {Websocket} ws Websocket Instance
+   * @param {Number} id - Shard's ID
    */
-  constructor (ws) {
-    if (!(ws instanceof Websocket)) throw new HwSharderError('')
+  constructor (ws, id) {
+    if (!(ws instanceof Websocket)) throw new HwSharderError('CLIENT_INSTANCEOF_WS')
     this._ws = ws
+    this.id = id
   }
 
   _registerEvents () {
